5199c56214c118484006bbd11fe28c39e6f11a39
mohamed amer <mohamedamer@yahoo.com>
2020-08-06 10:14:59 +0200

    frist page

---
+++ frist project/js/project.js
@@ -0,0 +1,36 @@
+ //trigger of frist
+ $('.frist-slider').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:3
+        }
+    }
+})
+//trigger the second slider
+$('.second-slider').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:4
+        }
+    }
+})
+
+
